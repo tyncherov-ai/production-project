b0496ca5e02ec438cd0b8c0bac0c82e33c318c72
Vagiz Tincherov <vagiztincherov@gmail.com>
2024-09-03 17:16:55 +0500

    second

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,38 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev
+                ? "style-loader"
+                : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: options.isDev
+                            ? '[path][name]__[local]--[hash:base64:8]'
+                            : '[hash:base64:8]'
+                    },
+                }
+            },
+            "sass-loader",
+        ],
+    }
+
+    // Если не изспользуем typescript - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+    return [
+        typescriptLoader,
+        cssLoader
+    ]
+}
