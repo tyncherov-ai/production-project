a95909bfaf891e6c0d8df6c12d22f3af4a3c8515
Vagiz Tincherov <vagiztincherov@gmail.com>
2025-03-30 23:47:17 +0500

    это дебильный typescriptLoader ломает всю сборку, я не знаю как это исправить

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -46,5 +46,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     },
   };
 
-  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, babelLoader, cssLoader];
 }
